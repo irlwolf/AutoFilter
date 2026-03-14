c75ae1ed8a286617d324032258364efcb30602af
Spider-Man <theirlwolf@gmail.com>
2026-03-14 12:17:53 +0530

    Update AUTH_CHANNEL and reqst_channel IDs

--- info.py
+++ info.py
@@ -44,10 +44,10 @@ LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002452376610'))  # Log channel i
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-1002409821863'))  # Bin channel id (make sure bot is admin)
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002470891127'))  # Notification of those who verify will be sent to your channel
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002452376610'))  # Premium logs channel id
-auth_channel = environ.get('AUTH_CHANNEL', '-1002378928961')  # Channel/Group ID for force sub (make sure bot is admin)
+auth_channel = environ.get('AUTH_CHANNEL', '-1003705154668')  # Channel/Group ID for force sub (make sure bot is admin)
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '').split()]
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1003675721060')  # Support group id (make sure bot is admin)
-reqst_channel = environ.get('REQST_CHANNEL_ID', '-1003675721060')  # Request channel id (make sure bot is admin)
+reqst_channel = environ.get('REQST_CHANNEL_ID', '-1003705154668')  # Request channel id (make sure bot is admin)
 AUTH_CHANNEL = [int(fch) if id_pattern.search(fch) else fch for fch in environ.get('AUTH_CHANNEL', '-1002378928961').split()]
 MULTI_FSUB = [int(channel_id) for channel_id in environ.get('MULTI_FSUB', '-1002493211643').split() if re.match(r'^-?\d+$', channel_id)]  # Channel for force sub (make sure bot is admin)
 
